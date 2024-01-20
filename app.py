bed431bf84963ad8f11140b08ae8cc93f2fca772
Ankit Kadiyan <kadiyanankit11@gmail.com>
2024-01-20 17:28:37 +0530

    Data Transformation

--- app.py
+++ app.py
@@ -3,15 +3,19 @@ from src.ML_Project.logger import logging
 from src.ML_Project.exception import CustomException
 from src.ML_Project.componenets.data_ingestion import DataIngestion
 from src.ML_Project.componenets.data_ingestion import DataIngestionConfig
-
+from src.ML_Project.componenets.data_transformation import DataTransformationConfig,DataTransformation
 
 if __name__=="__main__":
     logging.info("The Execution has started")
 
     try:
-        data_ingestion_config=DataIngestionConfig()
+        # data_ingestion_config=DataIngestionConfig()
         data_ingestion=DataIngestion()
-        data_ingestion.initiate_data_ingestion()
+        train_data_path,test_data_path=data_ingestion.initiate_data_ingestion()
+
+        # data_transformation_config=DataIngestionConfig()
+        data_transformation=DataTransformation()
+        data_transformation.initiate_data_transformation(train_data_path,test_data_path)
 
     except Exception as e:
         logging.info("Custom Exception")
--- src/ML_Project/utils.py
+++ src/ML_Project/utils.py
@@ -3,8 +3,10 @@ import sys
 from src.ML_Project.exception import CustomException
 from src.ML_Project.logger import logging
 import pandas as pd
+import numpy as np
 from dotenv import load_dotenv
 import pymysql
+import pickle
 
 load_dotenv()
 
@@ -30,3 +32,15 @@ def read_sql_data():
     
     except Exception as ex:
         raise CustomException(ex)
+    
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path,'wb') as file_obj:
+            pickle.dump(obj, file_obj)
+            
+    except Exception as e:
+        raise CustomException(e.sys)
